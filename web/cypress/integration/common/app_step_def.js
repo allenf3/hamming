91409f41f52558487ff027b11c0bf7043a4415ee
Fred Allen <allenf3@msn.com>
2021-12-08 16:49:08 -0600

    1 - Fixed an issue with the e2e test - fa

--- web/cypress/integration/common/app_step_def.js
+++ web/cypress/integration/common/app_step_def.js
@@ -6,7 +6,7 @@ Given('I am on the app', () => {
   cy.visit('/');
 });
 
-Given('I am on the {page} Page', (page) => {
+Given('I am on the {string} Page', (page) => {
   cy.visit(`/${page}`);
 });
 
