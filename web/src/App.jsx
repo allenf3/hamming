5a3f4ef11b2fde5f572131517e341991395c40aa
Fred Allen <allenf3@msn.com>
2021-12-08 16:52:28 -0600

    1 - Added buttons to home page - fa

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HammingInfo from './components/HammingInfo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <HammingInfo />
+        <div>
+          <button
+            className="main-nav-button"
+            type="button"
+          >
+            Learn
+          </button>
+          <button
+            className="main-nav-button"
+            type="button"
+          >
+            Practice
+          </button>
+          <button
+            className="main-nav-button"
+            type="button"
+          >
+            Reports
+          </button>
+        </div>
       </header>
     </div>
   );
