f60a27efce335a3e0f0fd3d46a1b0e83c5904564
Fred Allen <allenf3@msn.com>
2021-12-15 12:46:51 -0600

    1 - Centered buttons - fa

--- web/src/Home.jsx
+++ web/src/Home.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import './Home.css';
+import { Grid } from '@mui/material';
 
 function Home() {
   return (
     <div className="Home">
       <header className="App-header">
         <div>
-          <button
-            className="main-nav-button"
-            type="button"
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
           >
-            Learn
-          </button>
-          <button
-            className="main-nav-button"
-            type="button"
-          >
-            Practice
-          </button>
-          <button
-            className="main-nav-button"
-            type="button"
-          >
-            Reports
-          </button>
+            <Button
+              variant="contained"
+              className="main-nav-button"
+              type="button"
+            >
+              Learn
+            </Button>
+            <Button
+              variant="contained"
+              className="main-nav-button"
+              type="button"
+            >
+              Practice
+            </Button>
+            <Button
+              variant="contained"
+              className="main-nav-button"
+              type="button"
+            >
+              Reports
+            </Button>
+          </Grid>
         </div>
       </header>
     </div>
