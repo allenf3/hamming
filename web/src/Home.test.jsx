0713cc6a3319fed0c3b1a9d3431e8cb96078c55a
Fred Allen <allenf3@msn.com>
2021-12-17 09:29:57 -0600

    3 - Create tests for Home page navigation - fa

--- web/src/Home.test.jsx
+++ web/src/Home.test.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 import Home from './Home';
 
 test('renders navigation buttons', async () => {
-  render(<Home />);
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   expect(await screen.findByText('Learn')).toBeInTheDocument();
   expect(await screen.findByText('Practice')).toBeInTheDocument();
   expect(await screen.findByText('Reports')).toBeInTheDocument();
 });
 
 test('navigates to Learn when clicked', async () => {
-  render(<Home />);
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/learn" element="Learn about Hamming codes" />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   const learn = await screen.findByText('Learn');
   expect(learn).toBeInTheDocument();
   fireEvent.click(learn);
@@ -18,7 +33,14 @@ test('navigates to Learn when clicked', async () => {
 });
 
 test('navigates to Practice when clicked', async () => {
-  render(<Home />);
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/practice" element="Practice working with Hamming codes" />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   const practice = await screen.findByText('Practice');
   expect(practice).toBeInTheDocument();
   fireEvent.click(practice);
@@ -26,7 +48,14 @@ test('navigates to Practice when clicked', async () => {
 });
 
 test('navigates to Reports when clicked', async () => {
-  render(<Home />);
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/reports" element="View Reports" />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   const reports = await screen.findByText('Reports');
   expect(reports).toBeInTheDocument();
   fireEvent.click(reports);
