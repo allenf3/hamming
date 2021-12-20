3d53698ab7b291e1a96414e71eeb067353efeecc
Fred Allen <allenf3@msn.com>
2021-12-20 09:39:09 -0600

    15 - Removed App.css - fa

--- web/src/Home.jsx
+++ web/src/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
-import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider, Box } from '@mui/material';
 import { ClassNames } from '@emotion/react';
 
