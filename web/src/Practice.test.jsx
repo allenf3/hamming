fdbd42a9fc1ef855d65999c0dedeabe4ced8c9b0
Fred Allen <allenf3@msn.com>
2021-12-20 09:45:53 -0600

    15 - Added link to home from practice - fa

--- web/src/Practice.test.jsx
+++ web/src/Practice.test.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Reports from './Reports';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import Practice from './Practice';
 
 test('practice page renders correctly', async () => {
-  render(<Reports />);
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Practice />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   expect(await screen.findByText('Practice working with Hamming codes')).toBeInTheDocument();
+});
+
+test('can navigate from Practice to Home', async () => {
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Practice />} />
+        <Route path="/" element="Main page" />
+      </Routes>
+    </BrowserRouter>,
+  );
+  const homePage = await screen.findByText('Main page');
+  expect(homePage).toBeInTheDocument();
+  fireEvent.click(homePage);
+  expect(await screen.findByText('Main page')).toBeInTheDocument();
 });
