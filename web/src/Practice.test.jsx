5a7bb5cfb93f1fe013be6b7b593585d830d85b51
Fred Allen <allenf3@msn.com>
2021-12-23 11:04:22 -0600

    15 - Added practice link test - fa

--- web/src/Practice.test.jsx
+++ web/src/Practice.test.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router';
 import Practice from './Practice';
+import shouldLinkToHome from './testUtilities/shouldLinkToHome';
 
 test('practice page renders correctly', async () => {
   render(
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Practice />} />
-      </Routes>
+      <Practice />
     </BrowserRouter>,
   );
   expect(await screen.findByText('Practice working with Hamming codes')).toBeInTheDocument();
 });
 
-test('can navigate from Practice to Home', async () => {
-  render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Practice />} />
-        <Route path="/" element="Main page" />
-      </Routes>
-    </BrowserRouter>,
-  );
-  const homePage = await screen.findByText('Main page');
-  expect(homePage).toBeInTheDocument();
-  fireEvent.click(homePage);
-  expect(await screen.findByText('Main page')).toBeInTheDocument();
+test('home link present on practice page', async () => {
+  await shouldLinkToHome(Practice);
 });
