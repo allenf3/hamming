8a6b12813e9d86ac5cb4c5803dac1e4f02623a85
Fred Allen <allenf3@msn.com>
2021-12-29 15:41:12 -0600

    2 - Front end consumes data from api - fa

--- web/src/Practice.jsx
+++ web/src/Practice.jsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-
-const HammingBit = () => (
-  <button type="button" className="hammingBit">
-    1
-  </button>
-);
+import './Practice.css';
 
 function Practice() {
+  const [code, setCode] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/HammingCodes`);
+        setCode(data);
+      } catch (err) {
+        setError(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return (
+      <div>There was an error fetching the codes.</div>
+    );
+  }
+
   return (
-    <div>
+    <div className="main">
       <h1>Practice working with Hamming codes</h1>
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
-      <HammingBit />
+      <div>
+        {code.map((bit, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Button key={index} variant="contained" type="button" size="large" className="hammingBit">
+            { bit }
+          </Button>
+        ))}
+      </div>
       <Link to="/">Home</Link>
     </div>
   );
--- web/src/Practice.test.jsx
+++ web/src/Practice.test.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Practice from './Practice';
 import shouldLinkToHome from './testUtilities/shouldLinkToHome';
+
+const testCodeValid = ['1', '0', '1', '1', '1', '0', '1', '1', '0', '0', '0', '1', '0', '0', '0', '1'];
 
 test('practice page renders correctly', async () => {
   render(
@@ -14,12 +18,16 @@ test('practice page renders correctly', async () => {
 });
 
 test('sixteen bits visible on page', async () => {
+  const bitArraySize = 16;
+  const mockApi = new MockAdapter(axios);
+  mockApi.onGet(`${process.env.REACT_APP_BASE_API}/api/HammingCodes`).reply(200, testCodeValid);
+
   render(
     <BrowserRouter>
       <Practice />
     </BrowserRouter>,
   );
-  expect(screen.getAllByRole('button')).toHaveLength(16);
+  expect(await screen.findAllByRole('button')).toHaveLength(bitArraySize);
 });
 
 test('home link present on practice page', async () => {
