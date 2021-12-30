f622d826897efcab2be82ecd793d98cad066cafe
Fred Allen <allenf3@msn.com>
2021-12-30 16:31:33 -0600

    17 - Changed HammingGrid to a function - fa

--- web/src/components/Practice/HammingGrid/HammingGrid.jsx
+++ web/src/components/Practice/HammingGrid/HammingGrid.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import HammingBit from './HammingBit';
 
-const HammingGrid = ({ code }) => (
-  <div className="hammingGrid">
-    { code.map((bit, index) => (
+function HammingGrid({ code }) {
+  return (
+    <div className="hammingGrid">
+      { code.map((bit, index) => (
       // eslint-disable-next-line react/no-array-index-key
-      <HammingBit key={index} bit={bit} index={index} />
-    ))}
-  </div>
-);
+        <HammingBit key={index} bit={bit} index={index} />
+      ))}
+    </div>
+  );
+}
 
 HammingGrid.defaultProps = {
   code: [],
