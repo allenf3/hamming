8337b5dc39a481d28a68cadd9c470b7a39a50228
Fred Allen <allenf3@msn.com>
2022-01-13 11:07:48 -0600

    16 - Added utility to calculate a Hamming Code - fa

--- api/backend.Tests/HammingUtilitiesTests.cs
+++ api/backend.Tests/HammingUtilitiesTests.cs
@@ -13,5 +13,15 @@ namespace backend.Tests
             var randomBytes = HammingUtilities.GetRandomBytes(2);
             Assert.Equal(2, randomBytes.Length);
         }
+
+        [Fact]
+        public void CalculateHammingCode_ProperlyAdjustsBits()
+        {
+            var myBytes = new byte[] { 172, 127 };
+            var calculated = HammingUtilities.CalculateHammingCode(myBytes);
+
+            Assert.Equal(204, calculated[0]);
+            Assert.Equal(255, calculated[1]);
+        }
     }
 }
--- api/backend/HammingUtilities.cs
+++ api/backend/HammingUtilities.cs
@@ -11,5 +11,50 @@ namespace backend
             rand.NextBytes(bytes);
             return bytes;
         }
+
+        public static byte[] CalculateHammingCode(byte[] randomBytes)
+        {
+            if (randomBytes.Length == 2)
+            {
+                var groups = new[]
+                {
+                    new { Parity = 1, Area = new int[] { 3, 5, 7, 9, 11, 13, 15 } },
+                    new { Parity = 2, Area = new int[] { 3, 6, 7, 10, 11, 14, 15 } },
+                    new { Parity = 4, Area = new int[] { 5, 6, 7, 12, 13, 14, 15 } },
+                    new { Parity = 8, Area = new int[] { 9, 10, 11, 12, 13, 14, 15 } },
+                    new { Parity = 0, Area = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}}
+                };
+
+                int groupTotal = 0;
+                bool bitIsOn = false;
+
+                for (int i = 0; i < groups.Length; i++)
+                {
+                    foreach (var index in (groups[i].Area))
+                    {
+                        var workingByte = randomBytes[index / 8];
+                        var mask = (byte)(1 << (7 - (index % 8)));
+                        var placeholder = 1 << (7 - (index % 8));
+                        bitIsOn = ((workingByte & mask) == placeholder);
+                        groupTotal += bitIsOn ? 1 : 0;
+                    }
+
+                    var parityBitValue = (randomBytes[groups[i].Parity / 8] & (byte)(1 << 7 - (groups[i].Parity) % 8)) == 1 << 7 - (groups[i].Parity) % 8;
+                    if (groupTotal % 2 == 0 && parityBitValue)
+                    {
+                        randomBytes[groups[i].Parity / 8] = (byte)(randomBytes[groups[i].Parity / 8] ^ (1 << (7 - (groups[i].Parity % 8))));
+
+                    }
+                    if (groupTotal % 2 == 1 && !parityBitValue) // add else before if
+                    {
+                        randomBytes[groups[i].Parity / 8] = (byte)(randomBytes[groups[i].Parity / 8] ^ (1 << (7 - (groups[i].Parity % 8))));
+                    }
+
+                    groupTotal = 0;
+                    bitIsOn = false; //delete me
+                }
+            }
+            return randomBytes;
+        }
     }
 }
