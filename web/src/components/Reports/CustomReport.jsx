4c58a779e001cdda3f87b272eb23551c6d4a32be
Fred Allen <allenf3@msn.com>
2022-02-08 10:27:10 -0600

    7 - Differentiated between logged in and not logged in on reports page - fa

---
+++ web/src/components/Reports/CustomReport.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CustomReport() {
+  return (
+    <div>
+      <h2>You are authenticated</h2>
+    </div>
+  );
+}
+
+export default CustomReport;
--- web/src/components/Reports/Reports.jsx
+++ web/src/components/Reports/Reports.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import Welcome from '../sharedComponents/Welcome';
+import CustomReport from './CustomReport';
+import './Reports.css';
 
 function Reports() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div>
-      <h1>View Reports</h1>
+    <div className="main">
+      <div className="container heading">
+        <h1>View Reports</h1>
+        <Welcome />
+      </div>
+      <div className="report-area">
+        {isAuthenticated ? <CustomReport /> : 'You must be logged in to view this page'}
+      </div>
       <Link to="/">Home</Link>
     </div>
   );
